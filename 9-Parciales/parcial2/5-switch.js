e76f0941bfb6820bf6ebd63f3b012aca97591366
braian1722 <braian_1721@hotmail.com>
2018-02-19 11:31:47 -0300

    clase 4.0

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -1,7 +1,16 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 5-Switch");
+	var mesdelaño;
+	mesdelaño = prompt ("ingrese el mes");
+	switch (mesdelaño){
+		case "Enero":
+		case "Febrero":
+		      alert ("veranitoo");
+			  break;
+		default:
+			  alert("extraño enero y febrero");
+	}
 	
 }
 
