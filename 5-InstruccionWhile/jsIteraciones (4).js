97c073d0a5494d12e30fc7067de4584e86c98e34
braian1722 <braian_1721@hotmail.com>
2018-02-19 12:04:59 -0300

    clase 4 while

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 10.");
+    
+	var numero = prompt("ingrese un número entre 0 y 9.");
+	while (numero <0 || numero >9){
+		numero = prompt (" error..ingrese un numero entre 0 y 9.");
+		console.log(numero);
+		if (numero == null){
+			break;
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
