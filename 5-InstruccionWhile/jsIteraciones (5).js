ad2b72a96f72448c53200b1192d1b3e6c9c7b8cf
braian1722 <braian_1721@hotmail.com>
2018-02-19 12:22:12 -0300

    clase 4 whiteee

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -6,5 +6,11 @@ var sexo = prompt("ingrese f ó m .");
 
 
 document.getElementById('Sexo').value=sexo;
+while (sexo !="f" && sexo !="m"){
+    sexo = prompt ("error...ingrese f o m");
+    if(sexo ==null){
+        break;
+    }
+}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -8,5 +8,6 @@ function Mostrar()
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
+contador= prompt ("ingresar 5 numeros")
 
 }//FIN DE LA FUNCIÓN
