f9962d92b9703ab9aaf522dd5489b50f02d25c50
braian1722 <braian_1721@hotmail.com>
2018-02-27 11:47:20 -0300

    Signed-off-by: braian1722 <braian_1721@hotmail.com>

--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -4,7 +4,13 @@ function Mostrar()
 	var nota;
 	var sexo;
 	var alumnos;
-	
+	var acumulador= 0;
+	var contador = 0;
+	var minimo;
+	var maximo;
+	var contadorv=0;
+
+
 	nota = prompt ("ingrese notas del 0 al 10")
 	while (nota >10 || nota <0 ){
 		nota = prompt (" error ingrese la nota")
@@ -14,5 +20,24 @@ function Mostrar()
         sexo = prompt ("error .. ingrese sexo")
 	}
 	
+	contador ++;
+	acumulador += nota
+	alert (" promedio de notas mas bajas" + nota)
+
+	if (contador == 1){
+		minimo = nota
+		alert ("nota mas baja" + nota)
+	}
+	else
+	{
+		if(nota < minimo)
+		minimo  =nota;
+	}
+	if ( sexo =="m" && nota > 6){
+		contadorv ++
+		alert (" cantidad de baron con notas mayor a 6"+ nota)
+	}
+ 
+
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,17 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var numero;
+	var contadorpares = 0
+    var alumno;
+	var acumulador;
+
+	if (numero % 2 == 0){
+		contadorpares ++;
+	
+	alumno += contador
+	}
+
 	
 }
 
